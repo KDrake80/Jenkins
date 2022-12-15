19bf08751a8a00a101a5d24ad0c351dd8c1f383b
Kevin Drake <94082862+KDrake80@users.noreply.github.com>
2022-12-15 16:00:12 -0700

    Update test.java
    Made Changes to .java file

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
 public class test {
    public static void main(String[] args) {
-      System.out.println("Test: 1");
+      System.out.println("Test: 2: Changes to Java File");
+      System.out.println("I have changed my test.java file on git");
+      System.out.println("Used the 'javac' command on test.java");
+      System.out.println("Pushed the '.class' file to repo");
+      System.out.println("Execute Windows Batch Command on Jenkins   'java test' ");
    }
 }
